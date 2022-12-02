bcc1ed02d5f547d542e79914d1c998d47dc7cc95
Sofia Vaca Narvaja <sofia_vaca_narvaja@brown.edu>
2022-12-02 05:10:51 -0500

    vamos que se puede

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 import "bootstrap"
+import "./Countries - Flags"
+import Teams from "./teams.json"
+import TeamConstructor from "./teamsConst"
+
 
 function App() {
+
+  // State variable for filtering: continent 
+  const [SouthAmerica, setSouthAmerica] = useState(false)
+  const [NorthAmerica, setNorthAmerica] = useState(false)
+  const [Asia, setAsia] = useState(false)
+  const [Africa, setAfrica] = useState(false)
+  const [Europe, setEurope] = useState(false)
+
+  // State variable for filtering: number of cups won
+  const [Zero, setZero] = useState(false)
+  const [One, setOne] = useState(false) 
+  const [Two, setTwo] = useState(false)
+  const [Five, setFive] = useState(false)
+
+  // State Variable for sorting: alphabetically
+  const [sortLetter, setSortLetter] = useState("FirstLetter");
+
+  // State Variable for aggregator: favorites
+  const [favs, setFavs] = useState("TheBest")
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
---
+++ src/teamsConst.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+export default function TeamsCards(props) {
+
+    // Giving props a variable name to facilitate use
+    const country = props.country
+    const flag = props.flag
+    const continent = props.continent
+    const cups = props.cups
+
+    return (
+    <div> 
+    <img src={flag} alt="Country's Flag"/>
+    <div className="Country"> {country} </div>
+    <div className="Header"> Continent: </div>
+    <div className="Section"> {continent}</div>
+    <div className="Header"> Cups: </div>
+    <div className="Section"> {cups}</div>
+    </div>
+    );
+}
